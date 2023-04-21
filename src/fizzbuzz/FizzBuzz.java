aadb3934f3b275adb9c4b52e595d4edd92a9e64d
Wilkens Joseph <jsw3430a@ens.fsi>
2023-04-21 14:47:36 +0200

    exemple

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,15 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
+	private static final String UN = "1";
+	//private static final String DEUX= "2";
+	//private static final String FIZZ= "fizz";
+
+	public String fizzBuzz(int entree) {
+		if (entree==1)
+		return UN;
+		
+		return Integer.toString(entree);
+	}
+
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -2,13 +2,26 @@ package fizzbuzz;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	FizzBuzz fb;
+	
+	@BeforeEach
+	void setup() throws Exception{
+		fb= new FizzBuzz();
 	}
+	
+	
+	@Test
+	void test_fb1() {
+		assertEquals("1", fb.fizzBuzz(1));
+	}
+	
+	//indiquer les regressions
+	//on ne commit pas du code red
+	// on ne teste pas les fonctions privees
 
 }
